8d106b24ab12653428ff0b02ce864f987544ca1b
RH-Quadient <97501727+RH-Quadient@users.noreply.github.com>
2022-03-03 12:37:28 -0600

    Add files via upload

---
+++ Parse_engine/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using CsvHelper;
+using System.IO;
+using System.Globalization;
+using System.Linq;
+using CsvHelper.Configuration;
+using CsvHelper.Configuration.Attributes;
+
+namespace FinalParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var streamReader = new StreamReader(@"C:\Users\khoushid\Desktop\reporting AIMS Zacs file\@AdvancedReporting\TestFiles\smallsampleAddresslist.csv"))
+            {
+                using (var csvReader = new CsvReader( streamReader, CultureInfo.InvariantCulture))
+                {
+                    csvReader.Context.RegisterClassMap<CsvParseClassMap>();
+                    var records = csvReader.GetRecords<CsvParse>().ToList();
+                }
+            }
+
+        }
+    }
+}
